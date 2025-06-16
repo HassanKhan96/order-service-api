ff8c4f6bd5821ee55ae794b1bf3795b76617ec6e
ibrahim-coding <ik0891645@gmail.com>
2025-06-16 08:35:12 -0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,10 @@ import { CategoryModule } from './category/category.module';
 import { VariationModule } from './variation/variation.module';
 import { ChoicesModule } from './choices/choices.module';
 import { UsersModule } from './users/users.module';
-<<<<<<< HEAD
 import { EmailModule } from './email/email.module';
-=======
 import { OrderModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
 import { RefreshModule } from './auth/refresh.module';
-
->>>>>>> 573804301ea470cf49f63caeb0208b94d862ad45
 
 @Module({
   imports: [
@@ -25,15 +21,11 @@ import { RefreshModule } from './auth/refresh.module';
     CategoryModule,
     VariationModule,
     ChoicesModule,
-<<<<<<< HEAD
     UsersModule,
-=======
     OrderModule,
     UsersModule,
     AuthModule,
-    RefreshModule
-    
->>>>>>> 573804301ea470cf49f63caeb0208b94d862ad45
+    RefreshModule,
   ],
   controllers: [AppController],
   providers: [AppService],
