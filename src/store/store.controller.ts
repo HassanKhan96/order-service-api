926dcf6d8d7ed8f58b3ae4bacaf5e2d8c448afa2
ibrahim-coding <ik0891645@gmail.com>
2025-06-17 19:04:57 +0500

    code runnig fluently

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -1,30 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseGuards, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+  UseGuards,
+  Query,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { StoreService } from './store.service';
 import { CreateStoreDto } from './dto/create-store.dto';
 import { UpdateStoreDto } from './dto/update-store.dto';
 import { AuthGuard } from 'src/auth/AuthGuard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('store')
-@UseGuards(AuthGuard)
+ @UseGuards(AuthGuard)
 export class StoreController {
   constructor(private readonly storeService: StoreService) {}
-       
-  @Get("/GET")
-  getall(@Query() city:string){
-    return this.storeService.restaurentList(city)
-  }
-  @Post("/add")
-  newUser(@Body() body:CreateStoreDto){
-    return this.storeService.create(body)
 
+  @Get('/GET')
+  getall(@Query() city: string) {
+    return this.storeService.restaurentList(city);
   }
-  @Put("/update/:id")
-  update(@Body() body:CreateStoreDto,@Param("id") id:string){
-     return this.storeService.updateRestaurent(body,id)
+  @Post('/add')
+  @UseInterceptors(FileInterceptor('image'))
+  newUser(@Body() body: any, @UploadedFile() image: Express.Multer.File) {
+    console.log(image);
+    return this.storeService.create(body);
   }
-  @Delete("/delete/:id")
-  delete(@Param("id") id:string ){
-       return this.storeService.deleteRestaurent(id)
+  @Put('/update/:id')
+  update(@Body() body: CreateStoreDto, @Param('id') id: string) {
+    return this.storeService.updateRestaurent(body, id);
   }
- 
+  @Delete('/delete/:id')
+  delete(@Param('id') id: string) {
+    return this.storeService.deleteRestaurent(id);
+  }
 }
--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { StoreController } from './store.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StoreSchemaObject } from './schemas/store.schema';
+import { MulterModule } from '@nestjs/platform-express';
+import * as path from 'node:path';
+import { diskStorage } from 'multer';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports:[MongooseModule.forFeature([StoreSchemaObject])],
+  imports: [
+    MongooseModule.forFeature([StoreSchemaObject]),
+    MulterModule.register({
+       
+      storage: diskStorage({
+        destination:path.join(__dirname, '..', '..', 'uploads'),
+        filename: (req, file, callback) => {
+          const UniqueSufix = Date.now() + '-' + file.originalname;
+          callback(null, UniqueSufix);
+        },
+      }),
+    }),
+  ],
   controllers: [StoreController],
   providers: [StoreService],
 })
