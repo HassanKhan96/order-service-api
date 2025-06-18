885d0ae190bd113e2579a08479d7c5d765022522
ibrahim-coding <ik0891645@gmail.com>
2025-06-17 23:39:39 -0700

    this is checking

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,7 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { citiesServices } from './cities.services';
+import { citiesDto } from './dto/cities.dto';
+import { updateCityDto } from './dto/updarecities.dto';
 
 @Controller('cities')
 export class citiesController{
     constructor(private readonly citiesServices:citiesServices){}
-}
+
+    @Post('/create-cities')
+    create(@Body() cities:citiesDto){
+        return this.citiesServices.create(cities);
+    }
+
+    @Get('/')
+    getCities(){
+        return this.citiesServices.get()
+    }
+
+    @Put('update/:id')
+      update(@Param() id:string ,city:updateCityDto) {
+        return this.citiesServices.updateCity(id, city);
+
+      }
+
+      @Delete('delete/:id')
+      deleteCity( id:string){
+        return this.citiesServices.remove(id)
+      }
+    }
--- src/cities/cities.services.ts
+++ src/cities/cities.services.ts
@@ -1,6 +1,7 @@
 import { Injectable, Param } from '@nestjs/common';
-import { citiesModel } from './schema/cities.schema';
+import { citiesModel, city } from './schema/cities.schema';
 import { citiesDto } from './dto/cities.dto';
+import { updateCityDto } from './dto/updarecities.dto';
 
 @Injectable()
 export class citiesServices {
@@ -8,33 +9,46 @@ export class citiesServices {
 
   async create(city: citiesDto) {
     const addcity = await new this.cities(city).save();
-    if (!city) {
+    if (!addcity) {
       return { message: 'city not available' };
     }
     return { message: 'city added' };
   }
 
-  async deleteCity(city:citiesDto , @Param("id") id:string){
-  const deleteCity = await  this.cities.findByIdAndDelete(id);
-
-    if(!deleteCity){
-        return {message:'city can not be deleted'};
+  async get(){
+    const getCities = await this.cities.find(city)
+    if(!getCities){
+        return{message:'city cannot be found'}
     }
-    return {message:'city deleted'}
-}
+    return{message:'city found'}
+  }
 
-    async updateCity(city:citiesDto,@Param("id") id:string){
-        try{
-            const update = await this.cities.findByIdAndUpdate(id)
-            if(!update){
-                return {message:"city cannot be updated"}
-            }
-            return{message:"city updated"}
+  async deleteCity(id: string) {
+    const deleteCity = await this.cities.findByIdAndDelete(id);
 
-        }catch(error){
-            console.log(error)
-        }
+    if (!deleteCity) {
+      return { message: 'city can not be deleted' };
     }
+    return { message: 'city deleted' };
+  }
 
+  async updateCity(city: updateCityDto, id: string) {
+    try {
+      const update = await this.cities.findByIdAndUpdate(id, city);
+      if (!update) {
+        return { message: 'city cannot be updated' };
+      }
+      return { message: 'city updated' };
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  async  remove(id:string){
+  const remove = await this.cities.findByIdAndDelete(id)
+  if(!remove){
+    return {message:"could not delete"}
+  }
+  return {message:"city deleted"}
+  }
 }
