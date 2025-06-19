26aab5c9cb15565db1e673174783fd8f9f72e40a
ibrahim-coding <ik0891645@gmail.com>
2025-06-19 07:18:49 -0700

    up to date

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, } from '@nestjs/common';
 import { citiesServices } from './cities.services';
 import { citiesDto } from './dto/cities.dto';
 import { updateCityDto } from './dto/updarecities.dto';
@@ -18,9 +18,8 @@ export class citiesController{
     }
 
     @Put('update/:id')
-      update(@Param() id:string ,city:updateCityDto) {
-        return this.citiesServices.updateCity(id, city);
-
+      update(@Param('id')  id:string ,city:updateCityDto) {
+      return this.citiesServices.updateCity(id,{city})
       }
 
       @Delete('delete/:id')
